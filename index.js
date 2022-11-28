7a5650b6fb0c429d507812bdc13389e24ff8c3a2
martinGITHUBER <maroymart@gmail.com>
2022-11-27 21:33:15 -0500

    fixed some stuff

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const https = require("https");
 const path = require("path");
+const fs = require("fs");
+const axios = require("axios").default;
+
 const app = express();
 
-const config = require(path.join(__dirname, "config.json"));
+const ssl_config = {
+	key: fs.readFileSync("/etc/letsencrypt/live/melvin4life.com/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/melvin4life.com/cert.pem"),
+};
 
-const port = 80 || process.env.PORT;
+const port = 443 || process.env.PORT;
 
-app.listen(port, () => {
+https.createServer(ssl_config, app).listen(port, () => {
 	console.log(`Listening to *:${port}`);
 });
+
+const config = require(path.join(__dirname, "config.json"));
 
 app.use(express.static("public"));
 
@@ -16,14 +25,31 @@ app.get("/config.json", (req, res) => {
 	res.sendFile(path.join(__dirname, "config.json"));
 });
 
-app.get(`/${config.games_path.replace("/", "")}/:game/*`, (req, res) => {
+app.get("/games", (req, res) => {
+	res.status(301).redirect("/");
+});
+
+const game_request = async (req, res) => {
 	const game = config.games.find(g => g.path == req.params.game);
 
 	if (game == undefined) {
-		res.status(404);
-		res.send("Error 404 - Game not found");
+		res.status(404).send("Error 404 - Game not found");
 	} else {
-		res.status(200);
-		res.sendFile(path.join(__dirname, req.path.replace(req.params.game, game.path)));
+		const file_path = path.join(__dirname, req.path.replace(req.params.game, game.path));
+		if (fs.existsSync(file_path)) {
+			res.status(200).sendFile(file_path);
+		} else {
+			res.status(404).send("Error 404 - Resource not found");
+		}
+	}
+};
+
+app.get(`/${config.games_path.replace("/", "")}/:game`, async (req, res) => {
+	if (!req.url.endsWith("/")) {
+		res.status(301).redirect(req.url + "/");
+	} else {
+		await game_request(req, res);
 	}
 });
+
+app.get(`/${config.games_path.replace("/", "")}/:game/*`, game_request);
--- scrape.js
+++ scrape.js
@@ -3,15 +3,12 @@ const config = require("./config.json");
 import("website-scraper").then(scrape => {
   scrape = scrape.default;
   const options = {
-    urls: ['https://discord.com/app'],
-    directory: './games/discord'
+    urls: ['https://1v1.lol/'],
+    directory: './games/1v1'
   };
   
   (async () => {
     // with async/await
     const result = await scrape(options);
-  
-    // with promise
-    scrape(options).then((result) => {});
   })();
 });
