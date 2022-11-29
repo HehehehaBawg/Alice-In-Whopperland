9b19c460e425b63e8492cd9d36ce588ad9c8a60c
MartinTintin3 <maroymart@gmail.com>
2022-11-28 23:41:55 -0500

    better scraper

--- scrape.js
+++ scrape.js
@@ -1,14 +1,16 @@
 const config = require("./config.json");
 
+const args = process.argv.slice(2);
+
 import("website-scraper").then(scrape => {
-  scrape = scrape.default;
-  const options = {
-    urls: ['https://1v1.lol/'],
-    directory: './games/1v1'
-  };
-  
-  (async () => {
-    // with async/await
-    const result = await scrape(options);
-  })();
+	scrape = scrape.default;
+	const options = {
+		urls: [args[0]],
+		directory: args[1],
+	};
+
+	(async () => {
+		// with async/await
+		const result = await scrape(options);
+	})();
 });
